214dcb2f4bfb3e22919525f96793c252bf2b1528
Sebaxtian18 <zsebax.1994@gmail.com>
2024-08-20 09:51:33 -0500

    Se agrega un sistema de condicional switch

--- script.js
+++ script.js
@@ -34,13 +34,36 @@
 
 
 // //condicional con tablas de verdad (Conjuncion(Y - &&) y disyuncion(O -||))
-let salario = 4000;
-if(salario > 10000 && salario < 12000){
-    console.log("Es verdadero")
-}else if(salario == 11000 || salario <= 1000){
-    console.log("Es verdadero")
-}else if(salario < 10000 && salario == 5000){
-    console.log("El salario es minimo")
-}else{
-    console.log("El salario no cumple los estanndares.")
+//Para que sea una conjuncion total, todas las condiciones se tienen que cumplir, mas, para que una disyuncion se cumpla, al menos una de las condiciones debe de ser verdadera.
+// let salario = 4000;
+// if(salario > 10000 && salario < 12000){
+//     console.log("Es verdadero")
+// }else if(salario == 11000 || salario <= 1000){
+//     console.log("Es verdadero")
+// }else if(salario < 10000 && salario == 5000){
+//     console.log("El salario es minimo")
+// }else{
+//     console.log("El salario no cumple los estanndares.")
+// }
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+
+//Practica de switch
+let escuela = prompt("Ingrese la escuela a la que desea acceder. 1- para Lenguas, 2- para Ingenieria, 3- para Derecho")
+switch(escuela) {
+    case "1":
+        console.log("Bienvenido a la escuela de lenguas");
+        console.log("Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis voluptatibus praesentium, fugiat vitae mollitia sequi dignissimos impedit hic excepturi corrupti aspernatur nemo odio cupiditate in consequuntur molestias provident dolorum reprehenderit.")
+        break;
+    case "2":
+        console.log("Bienvenido a la escuela de ingenieria");
+        console.log("Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis voluptatibus praesentium, fugiat vitae mollitia sequi dignissimos impedit hic excepturi corrupti aspernatur nemo odio cupiditate in consequuntur molestias provident dolorum reprehenderit.")
+        break;
+    case "3":
+        console.log("Bienvenido a la escuela de derecho")    
+        console.log("Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis voluptatibus praesentium, fugiat vitae mollitia sequi dignissimos impedit hic excepturi corrupti aspernatur nemo odio cupiditate in consequuntur molestias provident dolorum reprehenderit.")
+        break;
+    default:
+        console.log("Los datos no coinciden")
+        break;        
 }
