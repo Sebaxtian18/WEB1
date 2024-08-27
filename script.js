18702491be6bb047d220fa5349c4c23261311619
Sebaxtian18 <zsebax.1994@gmail.com>
2024-08-27 10:05:11 -0500

    Se crea la rama ciclos

--- script.js
+++ script.js
@@ -19,8 +19,42 @@
 //     }
 // }
 
-let index = 10;
-while (index < 20) {
-    console.log("")
+// let index = 10;
+// while (index < 20) {
+//     let numeroUno = Number(prompt("Ingrese un número: "));
+//     let numeroDos = Number(prompt("Ingrese un número: "));
+//     let operacion = parseInt(prompt("Seleccione la operación: \n1 - Sumar \n2 - Resta \n3- Multiplicacion \n4- Dividir"))
+//     switch (key) {
+//         case value:
+            
+//             break;
+    
+//         default:
+//             break;
+//     }
+//     index++
+// }
+
+// let repetir = true; /*Centinela*/
+// let index = 0; // Contador para acumular las veces ejecutadas.
+// while (repetir) {
+//     let estado = prompt("Desea repetir el codigo: \n1- para Si. \n2- para No."); /*condicional de estado para el centinela*/ 
+//     if(estado == "2"){
+//         repetir = false;
+//         index++
+//     }
+//     console.log(index ); 
+// }
+
+
+let repetir = true;
+let index = 0;
+do{
+    let estado = prompt("Desea repetir el codigo: \n1- para Si. \n2- para No."); /*condicional de estado para el centinela*/ 
+    if(estado == "2"){
+        repetir = false;
+        index++;
+    }
+    console.log("Estado del ciclo: " + repetir);
     index++
-}
+}while (repetir);
