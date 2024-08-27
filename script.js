d6a4eb3edc943ada2f80acc142d8f407ddf9d379
Sebaxtian18 <zsebax.1994@gmail.com>
2024-08-27 08:55:54 -0500

    Ciclos

---
+++ script.js
@@ -0,0 +1,26 @@
+// for(let i /*index*/ = 0; i < 5; i++/*incremento de uno en uno*/){
+//     let numeroUno = Number(prompt("Ingrese un número: "));
+//     let numeroDos = Number(prompt("Ingrese un número: "));
+//     let operacion = parseInt(prompt("Seleccione la operación: \n1 - Sumar \n2 - Resta \n3- Multiplicacion \n4- Dividir"))
+
+//     switch (operacion){
+//         case 1:
+//             console.log("El resulrado de la suma es: " + (numeroUno + numeroDos));
+//             break;
+//         case 2:
+//             console.log(numeroUno - numeroDos);
+//             break;
+//         case 3:
+//             console.log(numeroUno * numeroDos);
+//             break;
+//         case 4:
+//             console.log(numeroUno / numeroDos);
+//             break;
+//     }
+// }
+
+let index = 10;
+while (index < 20) {
+    console.log("")
+    index++
+}
