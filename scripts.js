e4eddf21725fc866b7884eabb82027323ac0790d
Sebaxtian18 <zsebax.1994@gmail.com>
2024-09-10 10:11:49 -0500

    Rama Estructuras de datos

---
+++ scripts.js
@@ -0,0 +1,29 @@
+// function unaFuncion(){
+
+// }
+// let numeros = [1,"jaime",true,unaFuncion,5,6]
+// console.log(numeros[0]);
+// console.log(numeros[1]);
+// console.log(numeros[2]);
+// console.log(numeros[3]);
+// console.log(numeros[4]);
+// console.log(numeros[5]);
+// //El console imprime em pantalla la posición 5 porque el arreglo comienza acontar apartir del 0
+
+let numeros = [1, 5, 3, 6, 2, 8, 9, 10, 20, 4, 5, 5]
+if (numeros[0] == 1){
+    console.log("Los numeros son iguales");
+}
+switch (numeros[8]) {
+    case 20:
+        console.log("El numero en la posición 8 es el: " + numeros[8]);
+        
+        break;
+    case 3:
+        console.log("El numero en la posición 3 es el: " + numeros[2]);
+        
+        break;
+
+    default:
+        break;
+}
