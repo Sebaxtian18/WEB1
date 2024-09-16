49ba1c27d0550ba3dbd265158d145602339dc569
Sebaxtian18 <zsebax.1994@gmail.com>
2024-09-15 21:09:22 -0500

    Realizo el primer punto de tres ejercicios con ciclos nivel intermedio

--- script.js
+++ script.js
@@ -47,14 +47,52 @@
 // }
 
 
-let repetir = true;
-let index = 0;
-do{
-    let estado = prompt("Desea repetir el codigo: \n1- para Si. \n2- para No."); /*condicional de estado para el centinela*/ 
-    if(estado == "2"){
-        repetir = false;
-        index++;
+// let repetir = true;
+// let index = 0;
+// do{
+//     let estado = prompt("Desea repetir el codigo: \n1- para Si. \n2- para No."); /*condicional de estado para el centinela*/ 
+//     if(estado == "2"){
+//         repetir = false;
+//         index++;
+//     }
+//     console.log("Estado del ciclo: " + repetir);
+//     index++
+// }while (repetir);
+
+// Ejercicios de practica.
+
+// for(index = 1; index <=10; index++){
+//     console.log(index);   
+// }
+
+
+// let numero = parseInt(prompt("Ingrese un numero para establecer su tabla de multiplicar"));
+// let index = 0;
+// while(index < 10){
+//     index = numero * index;
+//         index++
+    
+    
+//     console.log(index);
+// } 
+
+//Ejercicios de practica pedidos a la innteligencia artificial. 
+
+const numeros = [12, 1, 45, 11, 13, 14, 46, 95, 32, 2, 13, 18, 1, 33];
+const sumaPares = (numeros) => {
+    let suma = 0
+    for(i = 0; i < numeros.length; i++){
+        if(numeros[i] % 2 === 0 ){
+            suma += numeros[i]
+        }
     }
-    console.log("Estado del ciclo: " + repetir);
-    index++
-}while (repetir);
+    return suma
+}
+console.log(sumaPares(numeros));
+
+
+for(j = 0; j < numeros.length; j++){
+    console.log(numeros)
+    break
+}
+
